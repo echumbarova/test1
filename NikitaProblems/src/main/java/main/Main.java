11a5eed52731bfa3890e6be74c4d1f3013e79f2c
echumbarova <ch_ek@list.ru>
2015-10-06 10:06:14 -0700

    New  problem stub

--- NikitaProblems/src/main/java/main/Main.java
+++ NikitaProblems/src/main/java/main/Main.java
@@ -11,10 +11,11 @@ public class Main {
 		System.out.println("========= SIMPLE =========");
 		System.out.println("\t1. Second biggest number");
 		System.out.println("\t2. Decomposition into prime numbers");
+		System.out.println("\t3. Polyndrom checker");
 		System.out.println();
 		System.out.println("========= INTERMEDIATE =========");
-		System.out.println("\t3. Nth biggest number");
-		System.out.println("\t4. Run-length string encoding");
+		System.out.println("\t4. Nth biggest number");
+		System.out.println("\t5. Run-length string encoding");
 		
 		Scanner scanner = new Scanner(System.in);
 		int problemNumber = scanner.nextInt();
---
+++ NikitaProblems/src/main/java/simple/PolyndromChecker.java
@@ -0,0 +1,17 @@
+package simple;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+
+import main.Problem;
+
+public class PolyndromChecker extends Problem{
+
+	@Override
+	public void runFromInput(Scanner scanner, boolean skipPrompts,
+			PrintStream out) {
+		// TODO Your code here.
+		
+	}
+
+}
