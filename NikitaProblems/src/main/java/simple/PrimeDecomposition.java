04a5edb7aece501d5c95d02a21e78e529b831131
Irlim <Irlimlol@192.168.0.26>
2015-10-08 00:45:06 +0300

    ещё порция обнов перед заменой for на while

--- NikitaProblems/src/main/java/simple/PrimeDecomposition.java
+++ NikitaProblems/src/main/java/simple/PrimeDecomposition.java
@@ -9,29 +9,29 @@ import main.Problem;
 
 public class PrimeDecomposition extends Problem {
 
-  @Override
-  public void runFromInput(Scanner scanner, boolean skipPrompts, PrintStream out) {
-    if (!skipPrompts) {
-      System.out.println("Enter the number:");
-    }
-    int number = scanner.nextInt();
-    if (number <= 0) {
-        out.println("Nan");
-        return;
-      }
-    List<Integer> result = new ArrayList<Integer>();
-    
-    for (int i = 2, limit = (int)Math.ceil(Math.sqrt(number)); i <= limit; ) {
-        if (number % i == 0) {
-            number = number / i;
-            limit = (int)Math.ceil(Math.sqrt(number));
-            System.out.print(i + " ");
-        }
-        else {
-            i++;
-        }
-    }
-    if (number > 1) System.out.println(number);
-  }
+	@Override
+	public void runFromInput(Scanner scanner, boolean skipPrompts,
+			PrintStream out) {
+		if (!skipPrompts) {
+			System.out.println("Enter the number:");
+		}
+		int number = scanner.nextInt();
+		if (number <= 0) {
+			out.println("Nan");
+			return;
+		}
+		List<Integer> result = new ArrayList<Integer>();
+
+		for (int i = 2, limit = (int) Math.ceil(Math.sqrt(number)); i <= limit;) {
+			if (number % i == 0) {
+				number = number / i;
+				limit = (int) Math.ceil(Math.sqrt(number));
+			} else {
+				i++;
+			}
+		}
+		result.add(i);
+		out.println(number);
+	}
 
 }
