1f650014e58e9abae554061f3f41d42f6ca2e296
Irlim <Irlimlol@192.168.0.26>
2015-10-08 21:41:12 +0300

    осталось вроде только сравнить их и в вывод написать чтоб выводил полидром или не полидром это слово

--- NikitaProblems/src/main/java/simple/PolyndromChecker.java
+++ NikitaProblems/src/main/java/simple/PolyndromChecker.java
@@ -1,23 +1,34 @@
 package simple;
 
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 import main.Problem;
 
 public class PolyndromChecker extends Problem {
 
-  @Override
-  public void runFromInput(Scanner scanner, boolean skipPrompts, PrintStream out) {
+	@Override
+	public void runFromInput(Scanner scanner, boolean skipPrompts,
+			PrintStream out) {
 		if (!skipPrompts) {
 			System.out.println("Enter a word:");
 		}
 		String word = scanner.nextLine();
-		if (word==null) {
+		if (word == null) {
 			out.println("Nan");
 			return;
+		}
+    String original = word;
+    String reverse = "";
+    int length = original.length();
 
-  }
+    for (int i = length - 1; i >= 0; i--) {
+        reverse = reverse + original.charAt(i);
+    }          
 
-}
+		out.println(reverse.toString());
+	}
 }
