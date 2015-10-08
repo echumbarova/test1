da9b808b6599956971a7247ffd906e0d0b681216
Irlim <Irlimlol@192.168.0.26>
2015-10-09 02:49:48 +0300

    чисто промежуток

--- NikitaProblems/src/main/java/simple/ExchangesMoney.java
+++ NikitaProblems/src/main/java/simple/ExchangesMoney.java
@@ -1,6 +1,8 @@
 package simple;
 
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import main.Problem;
@@ -18,5 +20,22 @@ public class ExchangesMoney extends Problem {
 			out.println("Nan");
 			return;
 		}
+		int n = number;	
+		int x = 0;
+		List<Integer> result = new ArrayList<Integer>();
+		int y = (n - 3* (x)) / 5;
+		while (x<n/3){
+			if (n % i == 0) {
+				n = n / i;
+				result.add(i);
+			} else {
+				i++;
+			}
+		}
+		if (n > 1) {
+			result.add(n);
+		}
+		out.println(result.toString());
 	}
+
 }
