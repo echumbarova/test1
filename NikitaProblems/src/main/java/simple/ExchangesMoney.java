53096138cec3b52bebd701c2f9dc4ea5419a399c
Irlim <Irlimlol@192.168.0.26>
2015-10-09 04:02:46 +0300

    тупняк2 продолжение херни с циклом

--- NikitaProblems/src/main/java/simple/ExchangesMoney.java
+++ NikitaProblems/src/main/java/simple/ExchangesMoney.java
@@ -21,16 +21,17 @@ public class ExchangesMoney extends Problem {
 			return;
 		}
 		int n = number;
+		int y;
 		List<Integer> result = new ArrayList<Integer>();
-		for (int x=0; n/3>0 ;x++) {
-			
-	
+		for (int x = 0; n / 3 > 0; x++) {
+			if (n - 3 * x / 5 > 0) {
+				y = (n - 3 * x) / 5;
 			}
-		
+		}
+
 		if (n > 1) {
 			result.add(n);
 		}
 		out.println(result.toString());
 	}
-
 }
