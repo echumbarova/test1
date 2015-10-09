993d5a138040d9067fb63f6d349c45be0c4c7ea6
Irlim <Irlimlol@192.168.0.26>
2015-10-09 06:46:15 +0300

    как-то так

---
+++ NikitaProblems/src/test/java/simple/FindSubstringTest.java
@@ -0,0 +1,58 @@
+package simple;
+
+import static org.junit.Assert.assertEquals;
+import main.Problem;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class FindSubstringTest {
+	Problem problem;
+
+	@Before
+	public void setUp() throws Exception {
+		problem = new PrimeDecomposition();
+	}
+
+	@Test
+	public void zeroTest() {
+		assertEquals("Incorrect results for empty string.", "Nan",
+				problem.run(""));
+	}
+	
+	@Test
+	public void singleLetterTest() {
+		assertEquals("Incorrect results for 'a'.", "This is polindrome.",
+				problem.run("a"));
+	}
+	
+	@Test
+	public void twoLetterTest() {
+		assertEquals("Incorrect results for 'bb'.", "This is polindrome.",
+				problem.run("bb"));
+	}
+	
+	@Test
+	public void twoLetterFalseTest() {
+		assertEquals("Incorrect results for 'bc'.", "This is not polindrome.",
+				problem.run("bc"));
+	}
+	
+	@Test
+	public void evenLetterTest() {
+		assertEquals("Incorrect results for 'abbchhcbba'.", "This is polindrome.",
+				problem.run("abbchhcbba"));
+	}
+	
+	@Test
+	public void evenLetterFalseTest() {
+		assertEquals("Incorrect results for 'abtchhcbba'.", "This is not polindrome.",
+				problem.run("abtchhcbba"));
+	}
+	
+	@Test
+	public void oddLetterTest() {
+		assertEquals("Incorrect results for 'hhhhhhaaaacaaaahhhhhh'.", "This is polindrome.",
+				problem.run("hhhhhhaaaacaaaahhhhhh"));
+	}
+}
